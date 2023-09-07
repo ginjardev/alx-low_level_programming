31db379afbc5e868e0ed12ca004fb8fd42da8aea
Ini <arthurshur@gmail.com>
2023-09-07 14:25:52 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The main method, prints a string
+ * Return: Zero
+ */
+int main(void)
+{
+	puts(char "Programming is like building a multilingual puzzle\n");
+	return (0);
+}
