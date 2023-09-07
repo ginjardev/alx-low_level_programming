52465691beefe019e7dce71d49c2c9e5ee8f919b
Ini <arthurshur@gmail.com>
2023-09-07 15:27:58 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print string using printf()
+ * Return: zero returned.
+ */
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(str);
+	return (0);
+}
