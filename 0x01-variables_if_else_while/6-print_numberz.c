1068b72a3945d18d7618ace0d374ce67730079be
Ini <arthurshur@gmail.com>
2023-09-08 16:03:04 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints number 0 - 9
+* Return: zero returned
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('1');
+	}
+	putchar('\n');
+}
