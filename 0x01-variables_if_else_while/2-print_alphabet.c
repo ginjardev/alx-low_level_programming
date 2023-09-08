519e92ff345599dea4b30ae1624ba6c70fa82f4a
Ini <arthurshur@gmail.com>
2023-09-08 14:30:29 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print alphabets in lowercase
+* Return: zero is returned
+*/
+
+int main(void)
+{
+	for (int i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
