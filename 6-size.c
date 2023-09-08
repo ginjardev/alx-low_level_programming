0ada8d56e12556b34b3623ebe2436169c97ed934
Ini <arthurshur@gmail.com>
2023-09-07 22:36:39 -0700

    size of types

--- 6-size.c
+++ 6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long llng;
 	float flt;
 
-	printf("Size of a char: %c byte(s)\n", sizeof(chr));
-	printf("Size of an int: %d byte(s)\n", sizeof(itr));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(lng));
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(llng));
-	printf("Size of a float: %f byte(s)\n", sizeof(flt));
+	printf("Size of a char: %lu byte(s)\n", sizeof(chr));
+	printf("Size of an int: %lu byte(s)\n", sizeof(itr));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(lng));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llng));
+	printf("Size of a float: %lu byte(s)\n", sizeof(flt));
 
 	return (0);
 }
