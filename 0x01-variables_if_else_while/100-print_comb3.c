b22cdfe7fb7db61ace93083b7a25a35e056d899d
Ini <arthurshur@gmail.com>
2023-09-11 19:06:16 -0700

    combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int main(void)
 	unsigned int i;
 	unsigned int j;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 1; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
