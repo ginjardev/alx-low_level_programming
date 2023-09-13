358c084a8c28771798bd87e8bf33080b52421a26
Ini <arthurshur@gmail.com>
2023-09-13 06:20:00 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - test print_alphabet() function
+* Return: 0
+*/
+
+int main(void)
+{
+
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints lowercase alphabets
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
