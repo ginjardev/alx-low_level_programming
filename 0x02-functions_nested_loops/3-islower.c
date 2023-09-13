cdf68d6083f9198abdbc7a3818dd8b145db30bad
Ini <arthurshur@gmail.com>
2023-09-13 12:08:40 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+* _islower - checks for lowercase characters
+* Return: an integer 1 or 0 is returned if lowercase or not
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
