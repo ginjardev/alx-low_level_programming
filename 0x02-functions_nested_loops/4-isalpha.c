96d0adea050068426371440af07c168a9f4f3488
Ini <arthurshur@gmail.com>
2023-09-13 13:14:50 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isalpha - check for uppercase alphabets
+* @c: charcter to be checked
+* Return: 1 or 0 returned
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
