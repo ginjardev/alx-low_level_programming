f1472f7028e3d0ed5082f9bc58d7d6205b9d6cc2
Ini <arthurshur@gmail.com>
2023-09-13 22:57:56 -0700

    add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - sum two integers
+* @n: first integer
+* @m: second integer
+* Return: sum of two integers
+*/
+
+int add(int n, int m)
+{
+	return (n + m);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - checks add()
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
