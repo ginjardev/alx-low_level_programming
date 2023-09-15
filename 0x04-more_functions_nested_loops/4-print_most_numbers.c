21d65f5dac0a6def0fb5369f97d21d663f5eb314
Ini <arthurshur@gmail.com>
2023-09-14 19:19:40 -0700

    print most nums.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 - 9 excl. 2 & 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
