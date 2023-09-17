10fd6816014d494e4edd833e675624ba8d6aaa3a
Ini <arthurshur@gmail.com>
2023-09-17 02:32:59 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * fizz_buzz - prints Fizz, Buzz, FizzBuzz & integers
+ * main - test fizz_buzz
  * Return: 0
  */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
 
 void fizz_buzz(void)
 {
@@ -13,14 +20,14 @@ void fizz_buzz(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("%s", "Fizz") ;
+			printf("%s ", "Fizz") ;
 		else if (i % 5 == 0)
-			printf("%s", "Buzz");
+			printf("%s ", "Buzz");
 		else if (i % 5 == 0 && i % 3 == 0)
-			printf("%s", "FizzBuzz");
+			printf("%s ", "FizzBuzz");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
