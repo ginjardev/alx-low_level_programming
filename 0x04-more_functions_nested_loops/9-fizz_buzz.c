8adef582b63140473a6672a96aa2e35e5326f4fb
Ini <arthurshur@gmail.com>
2023-09-17 04:00:55 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("%s ", "Fizz") ;
+			printf("%s ", "Fizz");
 		else if (i % 5 == 0)
 			printf("%s ", "Buzz");
 		else if (i % 5 == 0 && i % 3 == 0)
