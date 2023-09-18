060210a942a620c488b5357f26894bd5160eaa86
Ini <arthurshur@gmail.com>
2023-09-18 16:33:29 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strlen - return length of string
+ * @s - string input
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	
+	length = sizeof(s) - 1;
+
+	return length;
+}
+
