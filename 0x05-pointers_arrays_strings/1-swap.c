c2f8ba9253163fadc61ad1ffbec7432bdf4f1b3b
Ini <arthurshur@gmail.com>
2023-09-18 15:27:23 -0700

    swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of integers
+ * @a: first integer to swap
+ * @b: second integer to swap
+ * Return: void
+ */
+
+swap_int(int *a, int *b)
+{
+	int *n;
+
+	*n = &a;
+	*a = &b;
+	*b = &n;
+}
