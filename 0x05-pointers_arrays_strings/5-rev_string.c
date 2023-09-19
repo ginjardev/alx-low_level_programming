20b1a02da3febab678a74676973ffc925c00e1d1
Ini <arthurshur@gmail.com>
2023-09-18 19:19:51 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * rev_string - reverses string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		for (j = i; j >= 0; j++)
+		{
+			s[i] = s[j];
+		}
+
+		i++;
+	}
+}
