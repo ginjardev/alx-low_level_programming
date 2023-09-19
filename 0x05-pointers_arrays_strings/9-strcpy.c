3ba950e45d74e51cdc27b044b00ff8934a7e0959
Ini <arthurshur@gmail.com>
2023-09-18 21:45:30 -0700

    copy array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * *_strcpy - copies string from source
+ * @dest: destination array
+ * @src: source array
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (i >= 0)
+	{
+		dest[i] = src[i];
+
+		if ( src[i] == '\0')
+		{
+			return (dest);
+		}
+
+		i++;
+	}
