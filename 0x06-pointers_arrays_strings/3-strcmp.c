46638c3ab2840e56d00b27c1e5ee229ad37f9970
Ini <arthurshur@gmail.com>
2023-09-23 19:23:56 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,12 +17,12 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] != s2[i])
 		{
 			result = s1[i] - s2[i];
-			return result;
+			return (result);
 		}
 		else if (s1[i] == s2[i])
 		{
 			result = 0;
-			return result;
+			return (result);
 		}
 
 		i++;
