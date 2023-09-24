d249a49a83d872ee4e6316d88584c19601ce5822
Ini <arthurshur@gmail.com>
2023-09-23 21:05:42 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encode string into 1337
+ * @c: char parameter
+ * Return: encoded string
+ */
+
+char *leet(char *c)
+{
+	int i, j;
+	char replace[] = "aAeEoOtTlL4433007711";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			if (c[i] == replace[j])
+			{
+				c[i] = replace[j + 10];
+			}
+		}
+	}
+	return (c);
+}
