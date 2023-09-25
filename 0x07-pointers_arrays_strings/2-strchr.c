9dc05e53f83cc39bdb371c3ce92300677240d2a2
Ini <arthurshur@gmail.com>
2023-09-25 16:57:44 -0700

    number 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchar - finds character in a string
+ * @s: pointer parameter
+ * @c: char parameter
+ * Return: pointer returned
+ */
+
+char *_strchr(char *s, char c)
+{
+
+}
