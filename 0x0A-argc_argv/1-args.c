a0a68f97af02c11c2b3b19bcd9e3a8c731e2e1c0
Ini <arthurshur@gmail.com>
2023-10-01 07:27:45 -0700

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of CL arguments
+ * @argc: number of CL arguments
+ * @argv: CL array
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%d\n" argc);
+	return (0);
+}
