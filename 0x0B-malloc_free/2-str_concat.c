eb70547c94d88fa6fbf2d14a1c700b189567d0c6
Ini <arthurshur@gmail.com>
2023-10-03 19:41:47 -0700

    concat strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - concat two strings
+ * @s1: first string parameter
+ * @s2: second string parameter
+ * Return: NULL or pinter
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+	char *arr;
+
+	i = 0, j = 0, k = 0;
+
+
+	arr = (char *)malloc(sizeof(*s1 + *s2));
+
+	if (arr == NULL)
+		return (NULL);
+
+	while (s1[i] != '\0')
+	{
+		arr[k] = s1[i];
+		i++;
+		k++;
+	}
+	while (s2[j] != '\0')
+	{
+		arr[k] = s2[j];
+		j++;
+		k++;
+	}
+
+	return (arr);
+	free(arr);
+}
