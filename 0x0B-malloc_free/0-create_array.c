edafda0f46aac5b821f6d2d5fab8d8f66ea2b3fd
Ini <arthurshur@gmail.com>
2023-10-03 18:27:34 -0700

    aray of char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates array of chars
+ * @size: int array size parameter
+ * @c: char parameter
+ * Return: NULL or pointer to array
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int 1 = 0;
+
+	arr = malloc(sizeof(c) * size);
+
+	while (i < size)
+	{
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		arr[i] = c;
+		i++;
+	}
+
+	return (arr);
+}
