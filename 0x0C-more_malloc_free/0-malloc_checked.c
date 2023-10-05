457388e647c6be0c2c90a4442da18f019a19e5ff
Ini <arthurshur@gmail.com>
2023-10-05 15:35:17 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory with malloc
+ * @b: unsigned int parameter
+ * Return: void pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
