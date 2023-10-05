6233a3c04a361edf2fba77e629e5837648e596dc
Ini <arthurshur@gmail.com>
2023-10-05 16:41:57 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates and initialize memory with calloc
+ *
+ * @nmemb: Number of elements in the array.
+ * @size: Size of each element.
+ * Return: NULL if nmemb or size is equal to 0, or if malloc fails
+ *         else it returns a pointer to the array created.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
