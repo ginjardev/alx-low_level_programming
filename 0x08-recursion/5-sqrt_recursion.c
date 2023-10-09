92d4d5948ab0a5736a925d0282fcad0e8b164634
Ini <arthurshur@gmail.com>
2023-10-09 05:27:25 -0700

    find squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds squareroot of numbers
+ * @n: square parameter
+ * Return: squareroot
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (find_sqrt(n, 0));
+}
+
+
+/**
+ * find_sqrt - confirm square
+ * @n: n parameter
+ * @i: i parameter
+ * Return: squareroot or -1
+ */
+
+int find_sqrt(int n, int i)
+{
+	if (n == i * i)
+		return (i);
+	if (n < i * i)
+		return (-1);
+	return (find_sqrt(n, i + 1));
+}
