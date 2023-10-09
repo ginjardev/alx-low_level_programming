799c61bc16e7eaec995a885bac20ef21b626c196
Ini <arthurshur@gmail.com>
2023-10-09 02:09:31 -0700

    power of numbers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power of numbers
+ * @x: number to be raise to power
+ * @y: power parameter
+ * Return: power of number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (0);
+	if (y == 1)
+		return (y);
+
+	return x * _pow_recursion(x, y - 1);
+}
