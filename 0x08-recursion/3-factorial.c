36d11ef2551516502ff02433e8b8b1c245817757
Ini <arthurshur@gmail.com>
2023-10-09 00:31:21 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - factorials of numbers
+ * @n: number parameter
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return n * factorial(n - 1);
+}
