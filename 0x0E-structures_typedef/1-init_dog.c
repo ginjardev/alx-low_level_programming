de9448251e2ca5cf0255a3cc4b443249a31ff415
Ini <arthurshur@gmail.com>
2023-10-09 20:24:56 -0700

    initialize dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initialize dog struct
+ * @d: dog struct type
+ * @name: dog name parameter
+ * @age: dog age parameter
+ * @owner: owner parameter
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
