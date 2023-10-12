767bea62b3a2f104aac621842d2b528d7392ac85
Ini <arthurshur@gmail.com>
2023-10-12 16:46:05 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers with separator
+ * @separator: separator for numbers
+ * @n: number of numbers to print
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, j;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		j = va_arg(args, int);
+		printf("%d", j);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
+}
