d77ae2a2581dc0f61f0ea0fc5748a66d10ea2a4a
Ini <arthurshur@gmail.com>
2023-10-12 17:24:50 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
  * print_strings - prints strings
  * @separator: sep. parameter
@@ -17,10 +18,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char*);
-		printf("%s", args == NULL ? "(nil)" : args);
-		if (separator != NULL && i < n - 1)
+		str = va_arg(args, char *);
+
+		if (separator != NULL && i > 0)
 			printf("%s", separator);
+
+		if (str == NULL)
+			printf("nil");
+		else
+			printf("%s", str);
 	}
+
+	va_end(args);
 	printf("\n");
 }
