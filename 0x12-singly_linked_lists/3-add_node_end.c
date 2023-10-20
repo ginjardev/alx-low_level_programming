6e4ee453dfdeb89fe30c3960303566b4111dbccc
Ini <arthurshur@gmail.com>
2023-10-19 20:19:00 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - find length of a string
+ * @str: String passed to the function
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int length;
+
+	for (length = 0; str[length]; length++)
+		;
+
+	return (length);
+}
+
+/**
  * add_node_end - add new node to last node
  * @head: list_t object
  * @str: str parameter
@@ -8,30 +23,37 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *next;
+	list_t *new_node, *current;
+	char *string;
 
-	if (head == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->str = strdup(str);
-	new->len = _strlen(str);
-	new->next = NULL;
-
-	next = *head;
-
-	if (next == NULL)
+	string = strdup(str);
+	if (string == NULL)
 	{
-		*head = new;
-		return (new);
+		free(new_node);
+		return (NULL);
 	}
 
-	while (next->next != NULL)
-		next = next->next;
+	new_node->str = string;
+	new_node->len = _strlen(string);
+	new_node->next = NULL;
 
-	next->next = new;
-	return (new);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new_node;
+
+	return (new_node);
 }
+
+
