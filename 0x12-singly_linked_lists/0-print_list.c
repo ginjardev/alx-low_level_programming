ca9e79828567822fcd2df5a6a0eff4586ec9259b
Ini <arthurshur@gmail.com>
2023-10-19 18:20:08 -0700

    print list items

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,15 @@ size_t print_list(const list_t *h)
 	while (current_node != NULL)
 	{
 		if (current_node->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", current_node->len, current_node->str);
-		numbier++;
+		}
 		current_node = current_node->next;
+		number++;
 	}
 
 	return (number);
