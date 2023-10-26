72f03540e709da6dcd0868acd2a1c009f2c0736a
Ini <arthurshur@gmail.com>
2023-10-25 19:03:05 -0700

    free linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees linked list
+ * @head: linked list parameter
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t current_node;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		current_node = head->next;
+		free(head);
+		head = current_node;
+	}
+}
