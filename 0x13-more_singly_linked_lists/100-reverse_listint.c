e1f20e39107a49f66854a9f4dcb59aee19af9a74
Ini <arthurshur@gmail.com>
2023-10-25 21:54:14 -0700

    reverse linked list'

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * reverse - reverses linked list
+ * @head: linked list parameter
+ * Return: pointer to first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous, *next;
+
+	previous = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next:
+	}
+	*head = previous;
+
+	return (head);
+}
