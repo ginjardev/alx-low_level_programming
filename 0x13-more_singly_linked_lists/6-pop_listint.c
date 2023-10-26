0d8111da99c47bc7cd120d5fe3365ca67a1ace30
Ini <arthurshur@gmail.com>
2023-10-25 19:34:51 -0700

    pop list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node returns data
+ * @head: linked list
+ * Return: head node's data
+ */
+
+int pop_list(listint_t **head)
+{
+	int data;
+	listint_t *current_node;
+
+	if (*head == NULL)
+		return (0);
+
+	current_node = *head;
+	data = (*head)->n;
+	*head = (*head)->next;
+	free(current);
+
+	return (data);
+}
