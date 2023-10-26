7109eabbc5a3d7401b03af6e13707f0267167a11
Ini <arthurshur@gmail.com>
2023-10-25 19:53:11 -0700

    nth node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns nth node of linked list
+ * @head: linked list parameter
+ * @index: index of node
+ * Return: nth node of linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int a = 0;
+	listint_t = current_node;
+
+	current_node = head;
+	while (current_node != NULL && i < index)
+	{
+		current_node = current_node->next;
+		a++;
+	}
+
+	if (a != index)
+		return (NULL);
+
+	return (current_node);
+}
