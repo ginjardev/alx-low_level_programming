19019e547ef3226a88c7f5afade9b36e3e995403
Ini <arthurshur@gmail.com>
2023-10-25 18:18:59 -0700

    add to end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add node to end
+ * @head: linked list
+ * @n: int parameter
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t new_node, next_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	next_node = *head;
+
+	if (next_node == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (next_node != NULL)
+	{
+		next_node = next_node->next;
+	}
+
+	next_node->next = new_node;
+
+	return (new_node);
+}
