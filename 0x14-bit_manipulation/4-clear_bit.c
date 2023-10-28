b3d8d00d50a3a61ddf22be215663fccb649e686d
Ini <arthurshur@gmail.com>
2023-10-27 18:10:57 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
  * @index: index to clear bit
  * Return: 1 or -1
  */
-int int clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
