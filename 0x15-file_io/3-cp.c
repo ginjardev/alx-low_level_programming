3fe6b4f18598a99880519e814f66a767c6150f7b
Ini <arthurshur@gmail.com>
2023-10-30 13:36:37 -0700

    copy files

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * print_exit_code - prints exit code status
+ * @code: exit code
+ * @text: text to print
+ * @filename: name of file
+ * Return: void
+ */
+void print_exit_code(int code, const char *text, const char *filename)
+{
+	dprintf(STDERR_FILENO, text, filename);
+	exit(code);
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument list
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int src, dst, bytes_r, bytes_w;
+	char buffer[BUFFERSIZE];
+
+	if (argc != 3)
+		print_exit_code(97, "Usage: cp file_from file_to\n", argv[0]);
+
+	src = open(av[1], O_RDONLY);
+	if (src == -1)
+		print_exit_code(98, "Error: Can't read from file %s\n", argv[1]);
+
+	dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dst == -1)
+		print_exit_code(99, "Error: Can't write to %s\n", argv[2]);
+
+	while ((bytes_r = read(src, buffer, BUFFERSIZE)) > 0)
+	{
+		bytes_w = write(dst, buffer, bytes_r);
+		if (bytes_w == -1)
+			print_exit_code(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	if (bytes_r == -1)
+		print_exit_code(98, "Error: Can't read from file %s\n", argv[1]);
+
+	if (close(src) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
+		exit(100);
+	}
+	if (close(dst) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dst);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,13 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+#define BUFFERSIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_exit_code(int code, const char *text, const char *filename);
+
 #endif
 
 
