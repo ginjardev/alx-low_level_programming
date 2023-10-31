143e8c271f7dda5bea230b2eda32b58888d59eb1
Ini <arthurshur@gmail.com>
2023-10-31 14:09:02 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,32 +9,23 @@
 
 void print_to_98(int n)
 {
-
-	if (n == 98)
-		printf("%d ", n);
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
-			printf("%d", n);
-			if (n != 98)
-				printf(", ");
-			else
-				printf(" ");
+			printf("%d, ", n);
 			n = n + 1;
 		}
 	}
 	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
-			printf("%d", n);
-			if (n != 98)
-				printf(", ");
-			else
-				printf(" ");
+			printf("%d,", n);
 			n = n - 1;
 		}
 	}
+
+	printf("%d\n", n);
 
 }
