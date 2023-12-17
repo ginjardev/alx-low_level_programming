0635e873cdcb7587c77b43ccd99a7c4364f23ab3
Ini <arthurshur@gmail.com>
2023-12-17 02:09:36 -0800

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,26 +13,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *move, *new_node;
 	unsigned int counter = 0;
 
+	/*add node at beginning if necessary*/
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	move = *h;
+	while (move && counter < idx - 1)
+	{
+		move = move->next;
+		counter++;
+	}
+
+	/*check idx is not bigger than linked list size*/
+	if (counter != idx - 1)
+		return (NULL);
+
+	/*check if insertation is at list end*/
+	if (move->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node != NULL)
 		return (NULL);
 
 	new_node->n = n;
-	if (*h != NULL)
-		move = (*h);
-
-	while (move)
-	{
-		if (counter == idx)
-			break;
-
-		move = move->next;
-		counter++;
-	}
-
-	new_node->next = move;
+	new_node->next = move->next;
 	new_node->prev = move->prev;
-	move->prev = new_node;
+	move->next->prev = new_node;
+	move->next = new_node;
 
 	return (new_node);
 }
